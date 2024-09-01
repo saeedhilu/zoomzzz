ceb2478580da9251089e67d615b0d42ddf5890f3
SAEED NM <saeedmoozhikkal@gmail.com>
2024-09-01 18:14:07 +0530

    commit all the files

--- src/utils/Axiox.js
+++ src/utils/Axiox.js
@@ -6,7 +6,7 @@
   import { useNavigate } from 'react-router-dom';
 
   const instance = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'http://54.153.235.250/',
     timeout: 5000,
     headers: { 'Content-Type': 'application/json' },
   });
