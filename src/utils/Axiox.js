bc7b5ea39f09967275a7be1eb7b595685f4c72c4
SAEED NM <saeedmoozhikkal@gmail.com>
2024-09-02 13:55:00 +0530

    after change the logic of the search bar without pagination TODO paginations is pending

--- src/utils/Axiox.js
+++ src/utils/Axiox.js
@@ -5,8 +5,8 @@ import { clearAuth, updateToken } from '../redux/slices/authSlice';
 import { toast } from 'react-toastify';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
-  timeout: 90000,
+  baseURL: 'https://api.zoomzzz.live/',
+  timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
 
