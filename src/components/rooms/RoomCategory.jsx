1d6b3bb62672ad5701f6a83489cc41b54cd8b185
SAEED NM <150129271+saeedhilu@users.noreply.github.com>
2024-09-06 22:36:35 +0530

    Update RoomCategory.jsx

--- src/components/rooms/RoomCategory.jsx
+++ src/components/rooms/RoomCategory.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import image from '../../assets/menu.png'
 const RoomCategory = ({ category }) => {
   return (
     <div className=" bg-slate-100 p-4 shadow-lg rounded-sm  w-36">
       <div className="img-div w-10 m-auto">
-        <img src={category.image} alt="catogary image" />
+        <img src={image} alt="catogary image" />
       </div>
       <h2 className="font-semibold text-center">
-        {category.name}
+        {category}
       </h2>
     </div>
   );
